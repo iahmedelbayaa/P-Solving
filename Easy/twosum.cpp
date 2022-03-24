fbd0e334fe6a663cb5763ab3c517463b9ce03bd1
iahmedelbayaa <elbayaaahmed845@gmail.com>
2022-03-24 18:08:39 +0200

    psolving

---
+++ Easy/twosum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    vector<int> result;
+    unordered_map<int, int> items;
+
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (items.find(nums[i]) != items.end())
+        {
+            result.push_back(items[nums[i]]);
+            result.push_back(i);
+            return result;
+        }
+        items[target - nums[i]] = i;
+    }
+    return result;
+}
